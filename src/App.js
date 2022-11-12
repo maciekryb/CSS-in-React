3d07b245e44f76b2894299d2443895a800a6e45a
Maciej Rybak <maciek.rybak95@gmail.com>
2022-11-12 18:00:07 +0100

    styling part1

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import './App.css';
 import Text from './Text';
 
 class App extends Component {
+  state = {
+    underline: false,
+  };
+
   render() {
+    const text = 'Witaj na stronie';
+    let classes = '';
+    if (this.state.underline) classes += ' line';
+    if (text.length > 10) classes += ' big';
+
     return (
-      <div className='app'>
-        <h1>Witaj na stronie</h1>
+      <div
+        className='app'
+        onClick={() => this.setState({ underline: !this.state.underline })}>
+        <h1 className={classes}>{text}</h1>
         <Text />
       </div>
     );
